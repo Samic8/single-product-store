6b269a41f858c7915b84f16dbee006cfdb1ea7bb
Sam Dawson <sam.dawson181@gmail.com>
2019-09-08 15:12:57 +1000

    Show config on mix and match select

--- src/components/atoms/ImageRadioButton.tsx
+++ src/components/atoms/ImageRadioButton.tsx
@@ -4,17 +4,29 @@ interface Props {
   name: string;
   value: any;
   className?: string;
+  selectedValue: string;
+  onSelect: (a: string) => void;
 }
 
 const ImageRadioButton: FunctionComponent<Props> = ({
   name,
   value,
   className,
-  children
+  children,
+  selectedValue,
+  onSelect
 }) => {
   return (
     <div className={`inline-flex ${className}`}>
-      <input type="radio" name={name} id={`${value}-id`} value={value} hidden />
+      <input
+        type="radio"
+        name={name}
+        id={`${value}-id`}
+        value={value}
+        onChange={e => onSelect(e.target.value)}
+        checked={selectedValue === value}
+        hidden
+      />
       <label className="image-radio-button" htmlFor={`${value}-id`}>
         <div className="image-radio-button-inner">{children}</div>
       </label>
