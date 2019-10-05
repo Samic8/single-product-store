27948fc2d5edf6974a7dda3020bb6ee1a47f8b9d
Kurt Charlie Hutten <kurt.hutten@accelo.com>
2019-10-05 16:47:54 +1000

    Formatting

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,24 @@
-import React, { useEffect, useContext} from "react";
+import React, { useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { graphql, useStaticQuery } from "gatsby";
-import { Store } from './containers/store';
+import { Store } from "./containers/store";
 
 function Layout({ children }) {
-  const dispatch = useContext(Store.Dispatch)
-  
+  const dispatch = useContext(Store.Dispatch);
+
   useEffect(() => {
     dispatch({
       type: "INIT_STORE FROM LS",
-      payload: null,
-    })
-    window.addEventListener("beforeunload", (event) => dispatch({
-      type: 'PUT_STORE_IN_LS',
-      payload: null,
-    }));
-    return () => window.removeEventListener('beforeunload');
-  }, [])
+      payload: null
+    });
+    window.addEventListener("beforeunload", event =>
+      dispatch({
+        type: "PUT_STORE_IN_LS",
+        payload: null
+      })
+    );
+    return () => window.removeEventListener("beforeunload");
+  }, [dispatch]);
 
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
