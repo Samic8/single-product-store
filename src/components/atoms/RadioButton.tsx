391ad8390f9a0fd43f101dd06bdb40d69a870e17
Sam Dawson <sam.dawson181@gmail.com>
2019-10-05 17:02:49 +1000

    Use material radio button

--- src/components/atoms/RadioButton.tsx
+++ src/components/atoms/RadioButton.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { getActiveClasses } from "../../utility/active-classes";
+import { withStyles } from "@material-ui/core/styles";
+import Radio from "@material-ui/core/Radio";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 interface Props {
   name: string;
@@ -10,11 +12,21 @@ interface Props {
   disabled?: boolean;
   onClick?: (a: string) => void;
 }
-// TODO: Add some sort of selection animation
+
+const PurpleRadio = withStyles({
+  root: {
+    color: "#9B8FBC",
+    "&$checked": {
+      color: "#C0BCCE"
+    }
+  },
+  checked: {}
+})(props => <Radio color="default" {...props} />);
+
+// TODO: Animation seems to be stop abruptly and goes to the finished state on click.
 export default function RadioButton({
   name,
   value,
-  className,
   label,
   checked,
   disabled,
@@ -23,39 +35,33 @@ export default function RadioButton({
   const id = `${value}-${name}-id`;
 
   return (
-    <div className={`inline-flex items-center ${className}`}>
-      <input
-        type="radio"
-        name={name}
-        id={id}
-        value={value}
-        onChange={e => onClick(e.target.value)}
-        checked={checked}
-        hidden
-        disabled={disabled}
-      />
-      <label
-        className={getActiveClasses({
-          "radio-button": true,
-          "radio-button-disabled": disabled
-        })}
-        aria-label={name}
-        htmlFor={id}
-      >
-        <div className={"radio-button-inner"}></div>
-      </label>
+    <>
+      {!label && <StyledRadio />}
       {label && (
-        // TODO is this the right aria label?
-        <span
-          onClick={() => onClick(value)}
-          className={getActiveClasses({
-            "ml-1 text-white": true,
-            "cursor-pointer": !disabled
-          })}
-        >
-          {label}
-        </span>
+        <FormControlLabel
+          value={value}
+          className="text-white"
+          control={<StyledRadio />}
+          label={label}
+        />
       )}
-    </div>
+    </>
   );
+
+  function StyledRadio() {
+    return (
+      <PurpleRadio
+        id={id}
+        checked={checked}
+        onClick={e => {
+          onClick(e.target.value);
+        }}
+        value={`${value}`}
+        name={name}
+        color="primary"
+        disabled={disabled}
+        inputProps={{ "aria-label": `Variation ${value}` }}
+      />
+    );
+  }
 }
