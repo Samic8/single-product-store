62ad96785226b938336683c89224e2689238e71d
Sam Dawson <sam.dawson181@gmail.com>
2019-10-12 12:38:39 +1100

    Use match media for determining view type
    Having two left/right hand side components running was causing problems with unqiue id's for radio buttons.
    
    The mobile "display: hidden" left and right hand side where still being rendered and put into the dom, but were just visually hidden.
    
    The compromise with match media is that there will be a flash of content for mobile users.

--- src/components/containers/MainContent.tsx
+++ src/components/containers/MainContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SwipeableViews from "react-swipeable-views";
 
 import NavigationBar from "./NavigationBar";
@@ -10,6 +10,7 @@ interface Props {}
 
 export default function MainContent({  }: Props) {
   const [state, dispatch] = useStore();
+  const [viewType, setViewType] = useState<"desktop" | "mobile">("desktop");
   const index = state.activeNavBarTab === "config" ? 0 : 1;
   const onChangeIndex = updatedIndex => {
     dispatch({
@@ -17,18 +18,36 @@ export default function MainContent({  }: Props) {
       payload: updatedIndex === 0 ? "config" : "preview"
     });
   };
+
+  useEffect(() => {
+    const checkWidth = () => {
+      const mql = window.matchMedia(`(max-width: 1024px)`);
+      const newViewType = mql.matches ? "mobile" : "desktop";
+      setViewType(newViewType);
+    };
+
+    checkWidth();
+    window.addEventListener("resize", checkWidth);
+
+    return () => {
+      window.removeEventListener("resize", checkWidth);
+    };
+  }, []);
+
   return (
     <>
-      <div className="lg:hidden">
+      {viewType === "mobile" && (
         <SwipeableViews index={index} onChangeIndex={onChangeIndex}>
           <HomepageLeftHandSide className="mx-auto" />
           <HomepageRightHandSide />
         </SwipeableViews>
-      </div>
-      <div className="hidden lg:flex w-full relative">
-        <HomepageLeftHandSide className="mx-auto mx-0" />
-        <HomepageRightHandSide className="flex" />
-      </div>
+      )}
+      {viewType === "desktop" && (
+        <div className="flex w-full relative">
+          <HomepageLeftHandSide className="mx-auto mx-0" />
+          <HomepageRightHandSide className="flex" />
+        </div>
+      )}
       <NavigationBar />
     </>
   );
