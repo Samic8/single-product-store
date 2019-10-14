3a18af9ef981a290a7f1859e2e8a707348030345
Sam Dawson <sam.dawson181@gmail.com>
2019-10-14 19:04:02 +1100

    Make banner clickable

--- src/components/layout/HomepageRightHandSide.tsx
+++ src/components/layout/HomepageRightHandSide.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { Store } from "../../components/containers/store";
 import ThemeContainer from "../themes/Container";
 import SaveButton from "../SaveButton";
 import { getActiveClasses } from "../../utility/active-classes";
+import { useStore } from "../containers/store";
 
 interface Props {
   className?: string;
 }
 
 export default function HomepageRightHandSide({ className }: Props) {
-  const { selectedVariations, storeInfo } = React.useContext(Store.State);
-
+  const [{ selectedVariations, storeInfo }, dispatch] = useStore();
   return (
     <section
       className={getActiveClasses([
@@ -22,7 +21,10 @@ export default function HomepageRightHandSide({ className }: Props) {
         <div className="hidden lg:block">
           <SaveButton />
         </div>
-        <div className="bg-teal-100 text-teal-900 lg:rounded-sm px-3 py-2 flex-grow lg:mx-5 text-lg leading-snug">
+        <div
+          className="bg-teal-100 text-teal-900 lg:rounded-sm px-3 py-2 flex-grow lg:mx-5 text-lg leading-snug cursor-pointer"
+          onClick={() => dispatch({ type: "TOGGLE_SAVE_MODAL", payload: true })}
+        >
           <span className="font-bold">Single Product Store</span> is not
           available just yet.
           <div>Save your configuration so its ready to set live at launch</div>
